2a1981615a36d086cb0eadfbbcd244478d016d90
Charith J De Silva <charithjdesilva@gmail.com>
2023-05-04 21:45:14 +0530

    created hero introduction text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,19 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#ff4057]' />{/* create the little circle */}
           <div className='w-1 sm:5-80 h-40 redLogo-gradient' />{/* create the hr line below circle */}
         </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm&nbsp;
+            <span className='text-[#ff4057]'>Charith</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a fullstack developer,
+            <br className='sm:block-hidden' />
+             AI/ML learning enthusiast.<br /> I also have experiences in UIUX,<br className='sm:block-hidden' /> and 3D modelling, networking.
+          </p>
+        </div>
+
+        
       </div>
     </section>
   )
