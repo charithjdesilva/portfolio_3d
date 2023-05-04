165e5a6674870e0984e96c3defd118ce8ea1a433
Charith J De Silva <charithjdesilva@gmail.com>
2023-05-04 16:02:13 +0530

    modifeid font of Portfolio name span tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           window.scrollTo(0, 0);
         }} >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Charith J De Silva<span className='sm:block hidden'>| FullStack Developer</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Charith J De Silva<span className='sm:block font-semibold hidden'>| FullStack Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
