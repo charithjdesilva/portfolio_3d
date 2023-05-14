e9d4b89cc65a40540725a257ce476b91a163fb93
Charith J De Silva <charithjdesilva@gmail.com>
2023-05-14 11:40:08 +0530

    created email.js send function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,6 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-// 2YkMoJWcRWy8lA6-a
-// template_rjycyi4
-// service_n4ifa5f
-
 const Contact = () => {
   const formRef = useRef();
 
@@ -28,7 +24,39 @@ const Contact = () => {
     setForm({ ...form, [name] : value });
   }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_n4ifa5f',
+      'template_rjycyi4',
+      {
+        from_name: form.name,
+        to_name: 'Charith',
+        from_email: form.email,
+        to_email: 'charithjdesilva@gmail.com',
+        message: form.message,
+      },
+      '2YkMoJWcRWy8lA6-a'
+       )
+       .then(() => {
+        setLoading(false),
+        alert('Thank you! I will get back to you as soon as possible.');
+        
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        }, (error) => {
+          setLoading(false);
+
+          console.log(error);
+
+          alert('Something went wrong.');
+        });
+       })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
