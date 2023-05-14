2cc17a2045a1cabf976def6c186a53c3bfd701a2
Charith J De Silva <charithjdesilva@gmail.com>
2023-05-15 01:32:13 +0530

    added animation to myModel

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,24 +1,83 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { OrbitControls, Preload, useGLTF, useAnimations } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
-const Earth = () => {
-  // import the 3d model
-  const earth = useGLTF('./planet/scene.gltf');
+const Model = (props) => {
+  const group = useRef()
+  const { nodes, materials, animations } = useGLTF('./planet/MyModel3.glb')
+  const { actions } = useAnimations(animations, group)
+
+  useEffect(() => {
+    console.log(actions['Armature|mixamo.com|Layer0']);
+    actions['Armature|mixamo.com|Layer0'].play();
+  });
 
   return (
-    <primitive
-      object={earth.scene}
-      scale={2.5}
-      position-y={0}
-      rotation-y={0}
-    />
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene"
+      position={props.isMobile?[0, -3, 0]:[0, -2, 0]}
+      >
+        <hemisphereLight intensity={0.8}
+          groundColor="black" />
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={props.isMobile?3:2.5}>
+          <primitive object={nodes.mixamorigHips} />
+          <skinnedMesh name="EyeLeft" geometry={nodes.EyeLeft.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeLeft.skeleton} />
+          <skinnedMesh name="EyeRight" geometry={nodes.EyeRight.geometry} material={materials.Wolf3D_Eye} skeleton={nodes.EyeRight.skeleton} />
+          <skinnedMesh name="Wolf3D_Body" geometry={nodes.Wolf3D_Body.geometry} material={materials.Wolf3D_Body} skeleton={nodes.Wolf3D_Body.skeleton} />
+          <skinnedMesh name="Wolf3D_Hair" geometry={nodes.Wolf3D_Hair.geometry} material={materials.Wolf3D_Hair} skeleton={nodes.Wolf3D_Hair.skeleton} />
+          <skinnedMesh name="Wolf3D_Head" geometry={nodes.Wolf3D_Head.geometry} material={materials.Wolf3D_Skin} skeleton={nodes.Wolf3D_Head.skeleton} />
+          <skinnedMesh name="Wolf3D_Outfit_Bottom" geometry={nodes.Wolf3D_Outfit_Bottom.geometry} material={materials.Wolf3D_Outfit_Bottom} skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton} />
+          <skinnedMesh name="Wolf3D_Outfit_Footwear" geometry={nodes.Wolf3D_Outfit_Footwear.geometry} material={materials.Wolf3D_Outfit_Footwear} skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton} />
+          <skinnedMesh name="Wolf3D_Outfit_Top" geometry={nodes.Wolf3D_Outfit_Top.geometry} material={materials.Wolf3D_Outfit_Top} skeleton={nodes.Wolf3D_Outfit_Top.skeleton} />
+          <skinnedMesh name="Wolf3D_Teeth" geometry={nodes.Wolf3D_Teeth.geometry} material={materials.Wolf3D_Teeth} skeleton={nodes.Wolf3D_Teeth.skeleton} />
+        </group>
+      </group>
+    </group>
   )
 }
 
+// const Earth = () => {
+//   // import the 3d model
+//   const {nodes, materials, animations} = useGLTF('./planet/AnimatedMyModel.gltf');
+//   console.log(nodes);
+
+//   return (
+//     <primitive
+//       object={nodes}
+//       scale={2.5}
+//       position-y={0}
+//       rotation-y={0}
+//     />
+//   )
+// }
+
 const EarthCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // change the is mobile variable
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    // set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    // Add the callback function as a listner for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // remove the listner when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, []);
+
   return (
     <Canvas
       shadows
@@ -27,7 +86,7 @@ const EarthCanvas = () => {
       camera={{
         fov: 45,
         near: 0.1,
-        far: 200,
+        far: 100,
         position: [-4, 3, 6]
       }}
     >
@@ -39,7 +98,7 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI /2}
         />
 
-        <Earth />
+        <Model isMobile={isMobile} />
       </Suspense>
     </Canvas>
   );
