d20a73bdd418f73e113056b45ef7d7c832d66b3e
Charith J De Silva <charithjdesilva@gmail.com>
2023-09-01 23:10:52 +0530

    updated color theme for title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,14 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#ff4057]' />{/* create the little circle */}
+          <div className='w-5 h-5 rounded-full bg-[#4093ff]' />{/* create the little circle */}
           <div className='w-1 sm:5-80 h-40 redLogo-gradient' />{/* create the hr line below circle */}
         </div>
 
         <div className='flex flex-col md:flex-row'>
           <div>
             <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm&nbsp;
-              <span className='text-[#ff4057]'>Charith</span>
+              <span className='text-[#4093ff]'>Charith</span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               I am a fullstack developer,
