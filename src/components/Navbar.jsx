23801a456915d96891e7df5b3ed52511be8ebf18
Charith J De Silva <charithjdesilva@gmail.com>
2023-10-27 14:50:13 +0530

    updated navbar, added class NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full 
     flex-items-center py-5 fixed top-0 z-20 
-    bg-primary`}>
+    bg-primary navBar`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to="/" className='flex items-center gap-2'
         onClick={() => {
@@ -22,7 +22,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
             Charith J De Silva&nbsp;
-            <span className='sm:block font-normal hidden'>| Data Science Enthusiast</span></p>
+            <span className='sm:block font-normal hidden'>| Software Engineering Enthusiast</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
