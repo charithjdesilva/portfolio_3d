1b2dc319712731eda1687bcde573e312dc425898
Charith J De Silva <charithjdesilva@gmail.com>
2023-10-28 15:40:46 +0530

    added a scollbar for modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -16,6 +16,11 @@ const ProjectModal = ({ isOpen, project, onClose }) => {
     width: "75vw", // Set the modal's width to 75% of the viewport width
   };
 
+  const contentStyle = {
+    maxHeight: "50vh", // Set the maximum height for content to enable scrolling
+    overflowY: "auto", // Add a vertical scrollbar when content exceeds the maximum height
+  };
+
   return (
     <div
       className="modal-overlay fixed top-0 left-0 w-full h-full flex items-center justify-center"
@@ -31,7 +36,7 @@ const ProjectModal = ({ isOpen, project, onClose }) => {
               className="img-fluid inline max-w-30vw sm:max-w-70"
             />
           </div>
-          <div className="col-span-1 sm:col-span-1 flex flex-col items-center justify-center">
+          <div className="col-span-1 sm:col-span-1 flex flex-col items-center justify-center" style={contentStyle}>
             <div className="flex flex-row mb-2">
               <div
                 onClick={() => window.open(project.source_code_link, "_blank")}
