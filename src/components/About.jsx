e2d16ae511e9348a402840c50b40349b70821821
De Silva H.M.C.J. <64733230+charithjdesilva@users.noreply.github.com>
2025-07-17 10:24:25 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I am a software engineering enthusiast with experience in React and .NET specializing in web development. I have a strong understanding of theoretical concepts in software engineering. I can swiftly learn new technologies. I am equally passionate about exploring, integrating, and discovering emerging technologies including Computer Vision, and AI/ML. 
+        I am a software engineering enthusiast with experience in React and SpringBoot specializing in web development. I have a strong understanding of theoretical concepts in software engineering. I can swiftly learn new technologies.
 <br /> <br />
 I'm a critical thinker and a problem solver. I dedicate to what I learn. And my self moto is 
         "Do not learn, learn why we are learning it."
